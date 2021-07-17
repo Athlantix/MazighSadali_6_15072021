5f3aa07d485c4579e07e0545bae27051e80a0cea
Athlantix <mazigh.sadali@gmail.com>
2021-07-17 10:44:53 +0200

    bcrypt

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,6 @@
-
+const mysql = require('mysql');
 const con=require("../config_db/db_connect");  
+const bcrypt = require('bcrypt');
 
 exports.AllUsers=(req,res,next)=>{
     let sql="SELECT * FROM user";
@@ -31,24 +32,41 @@ exports.AllPublication=(req,res,next)=>{
   
 exports.signup=(req,res,next)=>{
     console.log(req.body);
-    let sql= "INSERT INTO user VALUES (NULL,?,?,?,?,?,2)";
-    let insert=[req.body.nom, req.body.prenom, req.body.email, req.body.password, req.body.poste];
-    
-    con.query(sql,insert,(err,result)=>{
-      if(err) throw res.status(400).json({ message: 'Utilisateur existant' });
-      res.status(200).json({ message: 'Utilisateur ajouté' });
-    })
+    let psdCrypt=req.body.password;
+    bcrypt.hash(psdCrypt, 10, function(err,psdCrypt) {
+      console.log(psdCrypt)
+      let sql= "INSERT INTO user VALUES (NULL,?,?,?,?,?,2)";
+      let insert=[req.body.nom, req.body.prenom, req.body.email, psdCrypt, req.body.poste];
+      
+      con.query(sql,insert,(err,result)=>{
+        if(err) throw res.status(400).json({ message: 'Utilisateur existant' });
+        res.status(200).json({ message: 'Utilisateur ajouté' });
+      })
+      // Store hash in your password DB.
+  });
+
   }
   
   
 exports.login=(req,res,next)=>{
     //console.log(req.body.email);
-    let sql= "SELECT email from user where email = ? ";
+
+
+      // result == true
+ 
+
+    let sql= "SELECT email,id,password from user where email = ?";
     let insert=[req.body.email];
     
     con.query(sql,insert,(err,result)=>{
-      if(err) throw err;
+      
+      if(err)  res.status(400).json({ message: 'faux'});
+     
       if(result[0]==undefined){ res.status(400).json({ message: 'Aucun utilisateurs confirmé' }); }
-      else{res.status(200).json({ message: 'Utilisateur confirmé' });}
+      bcrypt.compare(req.body.password, result[0].password, function(err, result) {
+      if(result){res.status(200).json({ message: 'Utilisateur confirmé'})}
+      else { res.status(400).json({ message: 'Aucuns utilisateurs ne correspond'})}
+      });
     })
+ 
   }
